bfe74f6bea5be68c2c47871d6edfc38a62a94af0
agrevster <138630000+agrevster@users.noreply.github.com>
2025-05-25 14:58:56 -0500

    Added custom keybinds for changing tabs

---
+++ lua/custom/plugins/tabs.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_keymap('n', '<M-l>', ':tabnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-h>', ':tabprevious<CR>', { noremap = true, silent = true })
+
+return {}
